d8d0510e7121aa7d1c614041130fbb4e0e78a93f
sangjae84 <dltkdwo0514@gmail.com>
2023-06-14 15:48:55 +0900

    리스트 응용

--- 01_리스트+응용.py
+++ 01_리스트+응용.py
@@ -9,6 +9,7 @@
 * extend : 리스트를 연결하여 확장
 * insert : 특정한 인덱스에 요소를 추가
 '''
+from builtins import reversed
 
 ### 기존 리스트에 요소 하나 추가하기
 
@@ -171,14 +172,214 @@ print("초코파이" in cookies)
 
 store = ["마제소바", "토리동", "부타동"]
 # while store:  # 리스트 -> False? -> 그 안에 요소가 없을 때...
-while True:
-    print(store)
-    order = input("무엇을 주문하시겠습니까? : ")
-    if order in store:
-        store.remove(order)  # 요소를 계속 제거...
-        print(order + "을(를) 드리겠습니다")
-    else:
-        print(order + "은(는) 현재 없는 메뉴입니다")
-    if len(store) == 0: # if not store:
-        break
-print("장사 끝났습니다")
+# while True:
+#     print(store)
+#     order = input("무엇을 주문하시겠습니까? : ")
+#     if order in store:
+#         store.remove(order)  # 요소를 계속 제거...
+#         print(order + "을(를) 드리겠습니다")
+#     else:
+#         print(order + "은(는) 현재 없는 메뉴입니다")
+#     if len(store) == 0: # if not store:
+#         break
+# print("장사 끝났습니다")
+
+
+### 특정 값의 개수 구하기
+cookies = ['마카다미아 쿠키', '치즈 쿠키', '오레오 쿠키', '치즈 쿠키', '레드벨벳 쿠키', '치즈 쿠키']
+# 쿠키의 개수
+print('cookies.count("치즈 쿠키")', cookies.count("치즈 쿠키"))  # 정확하게 일치하는지
+song = "'마카다미아 쿠키', '치즈 쿠키', '오레오 쿠키', '치즈 쿠키', '레드벨벳 쿠키', '치즈 쿠키'"
+print(song.count("쿠키"))  # 문자열(string) -> 특정한 단어가 몇 개 존재하는지?
+
+### 리스트의 뒤집기
+print(cookies)
+print(cookies[::-1])  # 슬라이스 음수 증가폭
+print(list(reversed(cookies)))  # reversed
+print(cookies)  # 원본에 반영이 안됨 (뒤집었다는 것)
+cookies.reverse()  # 리스트객체.reverse()
+print("cookies.reverse() :", cookies)
+print(cookies)
+
+### 리스트의 요소 정렬
+'''
+sort()는 리스트의 요소을 작은 순서대로 정렬합니다(오름차순).
+
+sort() 또는 sort(reverse=False): 리스트의 값을 작은 순서대로 정렬(오름차순)
+sort(reverse=True): 리스트의 값을 큰 순서대로 정렬(내림차순)
+'''
+import random
+r_number = random.choices(range(1000), k=10)
+print(r_number)
+print(sorted(r_number))  # reversed -> 뒤집어진, sorted -> 정렬된
+print("<오름차순>")
+for r in sorted(r_number):
+    print(r)
+# 오르막 -> 오름차순. (ascending) - asc (데이터가 등장 혹은 전개되는 방향)
+# 1, 2, 3....
+# 내리막 -> 내림차순  (descending) - desc
+# 100, 99, 98, ...
+print(sorted(r_number)[::-1])
+print(sorted(r_number, reverse=True))  # sorted 정렬 -> 오름차순 -> 뒤집겠니?
+print(list(reversed(sorted(r_number)))) # reversed -> print -> list를 묶어줘야함
+# reversed(함수) & 리스트.reverse()
+# sorted(함수) & 리스트.sort()
+r_number = random.choices(range(1000), k=10)
+print("r_number", r_number)
+r_number.sort()
+print("r_number.sort()", r_number)  # 오름차순
+
+r_number = random.choices(range(1000), k=10)
+print("r_number", r_number)
+r_number.sort(reverse=True)
+print("r_number.sort(reverse=True)", r_number)  # 내림차순
+
+### 리스트의 모든 요소 삭제
+print("cookies", cookies)
+cookies.clear()
+print("cookies", cookies)
+
+### 리스트의 할당과 복사
+'''
+어떠한 값을 새로운 곳에 복사하는 방법
+1. 할당 (assignment)
+2. 얕은 복사 (shallow copy)
+3. 깊은 복사 (deep copy)
+'''
+a = [0] * 5
+print("a", a)
+b = a  # b에 a를 '할당'
+print("b", b)
+a[0] = 100
+a[1] = 150
+print("b", b)
+print(a is b)  # 2개가 같습니까?
+c = a[:]  # 슬라이스로 복사한 경우에는 할당이 아니라 '얕은 복사'이므로 서로 영향을 미치지 X
+print(a is c)  # 2개의 주소가 같습니까?
+a[0] = 1200
+a[1] = 100
+a[2] = 50
+print(c)
+d = a.copy() # 얕은 복사.
+print(c is d)
+
+e = [a, b, c] # 리스트를 담은 리스트.
+f = e.copy() # e[:] -> 얕은 복사
+print("e is f :", e is f)
+print(f)
+a[0] = 1338
+print(f)
+
+import copy  # copy
+g = copy.deepcopy(e)
+print("g", g)
+a[0] = 21381238
+print("g", g)
+
+# 1. 할당 - 얕은 복사 : 할당 -> 특정한 데이터를 저장한 주소를 넘기는 것.
+# 2. 얕은 복사 -> 같은 데이터지만, 다른 주소를 가지도록 사본을 만드는
+# (내부에 들어있는 주소까지 연결을 끊어주진않아요)
+# 3. 깊은 복사 (import copy.deepcopy) -> 모든 주소들의 연결을 끊어버려서 사본을 만드는 것.
+
+# 리스트(튜플) 가장 작은 수, 가장 큰 수, 합계
+# 가장 작은 수, 가장 큰 수
+
+# 1. 정렬
+a = random.choices(range(1, 1000), k=4)
+print("a", a)
+a.sort()
+print("a", a)
+print("최소값", a[0])
+print("최대값", a[-1])
+
+# 2. 반복 -> 모든 요소 중에 가장 크거나, 작은 값... 비교
+b = random.choices(range(1, 1000), k=10)  # 이중에 가장 큰 값을 구하세요 (sort 쓰지 않고)
+n = 0  # 외부에 저장된 값
+for v in b:
+    print("n:", n, "v:", v)
+    # 최대값을 구하는 방법
+    # if n < v:
+    #     n = v  # 더 큰 값을 n에 대입해서...
+    # 최소값...
+    if n == 0 or n > v:
+        n = v
+# 전체 반복을 끝내고 나면...
+print(b, n)
+
+# 3. 함수.
+# 최대값 : maximum -> max
+# 최소값 : minimum -> min
+print(max(b))
+print(min(b))
+
+# 합계
+s = 0
+for i in range(1, 10): # 1 ~ 9.
+    s += i
+print(s)
+# summarization -> sum
+print(sum(range(1, 10)))
+print(sum([329382, 129312328, 1231231]))
+
+s = ["바나나", "알러지", "원숭이"]
+# sum(s) # unsupported operand type(s) for +: 'int' and 'str' -> 0?
+# 문자열들은 sum을 쓸 수 X.
+c = ""
+for a in s: # 1 ~ 9.
+    c += a
+print(c)
+
+# 리스트 표현식 (리스트 컴프리헨션 - list comprehension)
+## -> 리스트 안에 for 반복문과 if 조건문으로 -> 값들의 묶음을 표현.
+## -> 리스트 안에 식, for 반복문, if 조건문 등을 지정하여 리스트를 생성 = 리스트 컴프리헨션
+## [식 for 변수 in 시퀀스]
+
+a = []
+for i in range(10):
+    a.append(i ** 2)
+print(a)
+a = [i ** 2 for i in range(10)]
+print(a)
+a = [i ** 0.5 for i in range(5, 10, 2)]
+print(a)
+a = [k ** 3.5 for k in range(3, 12, 3)]
+print(a)
+b = ["아메리카노", "카푸치노", "프라푸치노"]
+# c = []  # 맨 끝 2자리 옮기기
+# for v in b:
+#     c.append(v[-2:])
+# print(c)
+# c = [v[-2:] for v in b]
+c = [v + " 나왔습니다!" for v in b]
+print(c)
+
+
+# 리스트 컴프리헨션 + if 조건문
+'''
+[식 for 변수명 in 리스트 if 조건식]  # if 조건식을 만족시키는 값만, 식으로 표현
+'''
+d = []
+for v in b:
+    # print(v)
+    if "푸" in v:
+        # print(v)
+        d.append(v)
+print("d", d)
+e = [v for v in b if "푸" in v]
+print("e", e)
+
+# 조건부 표현식 (삼항연산자) -> for 앞에 (식)
+e = ["나 단 거 좋아해" if v == "프라푸치노" else "나 단 거 싫어해" for v in b if "푸" in v]
+print("e", e)
+# 리스트 컴프리헨션의 if문은 -> 만족을 안 시키면 필터링. -> continue -> len이 바뀜 !!!!
+# else가 없음...
+# 조건부 표현식(삼항연산자)는 조건의 T/F와는 상관없이 각 행이 여전히 존재. => 값. -> len 안바뀜
+# else가 있음...
+
+# 이중 for문
+for i in range(10): # i <- 안 겹치게만.
+    for j in range(10):  # 시퀀스에서 값을 빼내올 때 쓰는 변수명이 안겹치게만 조심. j
+        print(i, j, i * j)
+        # 삼중... 사중...
+# 이중 for문? 이중 리스트 컴프리헨션
+print([(i, j, i * j) for j in range(0, 10, 2) for i in range(0, 10, 3)])
